7e2826dcaaf418152988fae547c1122adacc2310
Carlos Neto <carlos.neto@flex.com>
2022-05-26 11:41:06 -0400

    Configurado o repository base

---
+++ LivrariaControleEmprestimo.DATA/Repositories/RepositoryBase.cs
@@ -0,0 +1,85 @@
+﻿using LivrariaControleEmprestimo.DATA.Interfaces;
+using LivrariaControleEmprestimo.DATA.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LivrariaControleEmprestimo.DATA.Repositories
+{
+    public class RepositoryBase<T> : IRepositoyModel<T>, IDisposable where T : class
+    {
+        protected p_StarTempContext _Contexto;
+        public bool _SaveChanges = true;
+
+        public RepositoryBase(bool saveChanges = true)
+        {
+            _SaveChanges = saveChanges;
+            _Contexto = new p_StarTempContext();
+        }
+
+        public T Alterar(T objeto)
+        {
+            _Contexto.Entry(objeto).State = EntityState.Modified;
+
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+
+            return objeto;
+        }
+
+        public void Dispose()
+        {
+            _Contexto.Dispose();
+        }
+
+        public void Excluir(T objeto)
+        {
+
+            _Contexto.Set<T>().Remove(objeto);
+
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+        }
+
+        public void Excluir(params object[] variavel)
+        {
+
+            var obj = SelecionarPk(variavel);
+            Excluir(obj);
+
+        }
+
+        public T Incluir(T objeto)
+        {
+            _Contexto.Set<T>().Add(objeto);
+
+            if (_SaveChanges)
+            {
+                _Contexto.SaveChanges();
+            }
+
+            return objeto;
+        }
+
+        public void SaveChanges()
+        {
+            _Contexto.SaveChanges();
+        }
+
+        public T SelecionarPk(params object[] variavel)
+        {
+            return _Contexto.Set<T>().Find(variavel);
+        }
+
+        public List<T> SelecionarTodos()
+        {
+            return _Contexto.Set<T>().ToList();
+        }
+    }
+}
